73f41fc98f084d5068510581bbc4a6ecb1200e25
Casey Drummer <caseydrummer@gmail.com>
2015-06-23 13:42:42 -0400

    Initial Commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to gen-manual-synth
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/gen_manual_synth/core_test.clj
@@ -0,0 +1,7 @@
+(ns gen-manual-synth.core-test
+  (:require [clojure.test :refer :all]
+            [gen-manual-synth.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
